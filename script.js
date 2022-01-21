9d9db75e54f02bf99538b231768acc1b4c429596
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-21 19:35:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -173,15 +173,13 @@ dt2 = new Date("October 13, 2014 11:13:00");
 console.log(diff_hours(dt1, dt2));
 
 
-
-
-
 // Write a JavaScript function to get time differences in days between two dates.   
 // Test Data :
 // dt1 = new Date("October 13, 2014 08:11:00");
 // dt2 = new Date("October 19, 2014 11:13:00");
 // console.log(diff_days(dt1, dt2));
 // 6
+
 
 
 function diff_days(dt2, dt1) 
